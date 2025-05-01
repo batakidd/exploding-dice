860b0798f9ae97fd3980ccc5f36c0ac1b51ec2d2
batakidd <107806599+batakidd@users.noreply.github.com>
2025-05-01 16:22:55 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,3 @@
-Hooks.on("ready", () => {
-  Hooks.on("rollComplete", (roll, context) => {
-    if (game.system.id === "age-of-sigmar-soulbound") {
-      applyExplodingDice(roll);
-    }
-  });
+Hooks.on("ready", function() {
+  console.log("Exploding Dice module is loaded!");
 });
-
-function applyExplodingDice(roll) {
-  roll.dice.forEach(die => {
-    if (die.total === 6) {
-      let rerolled = new Die("d6");
-      rerolled.roll();
-      die.total += rerolled.total;
-      ui.notifications.info(`Exploding die! Added ${rerolled.total} to the roll.`);
-    }
-  });
-}
